3f9bad26fb0f9e9f38c4be848f077ce42dc61215
sadblondeboi <bezawka@gmail.com>
2020-09-14 12:02:23 +0200

    event.component css fixes

--- src/app/views/event/event.component.ts
+++ src/app/views/event/event.component.ts
@@ -11,7 +11,7 @@ export class EventComponent implements OnInit {
     name : 'EUROPA TOUR',
     artist : 'Taco Hemingway',
     url : 'https://i2.wp.com/polskaplyta-polskamuzyka.pl/wp-content/uploads/2020/09/Taco-Hemingway-Europa.jpg?resize=1200%2C1200',
-    place: 'Poznań',
+    place: 'Poznań, gdzieś tam',
     date: '19/06/2020',
     time: '19:00',
     ticketPrice : '129'
